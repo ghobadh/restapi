05fdbdcf710280f3bca27cd921aa96c89a4790fe
ghobadh <ghobadh@gmail.com>
2024-11-07 10:03:05 -0500

    ResponeEntity Class has been added to the student controller.

--- src/main/java/ca/gforcesoftware/restapi/controller/StudentController.java
+++ src/main/java/ca/gforcesoftware/restapi/controller/StudentController.java
@@ -54,52 +54,67 @@ public class StudentController {
         return students;
     }
 
+    //This is similar to the other method in above but I try to embrace the response with ResponseEntity class
+    @GetMapping("/students_response")
+    public ResponseEntity<List<Student>> getStudents_response(){
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(1L, "John", "Doe"));
+        students.add(new Student(2L, "Jane", "Doe"));
+        students.add(new Student(3L, "Jack", "Gargamel"));
+        students.add(new Student(4L, "Parker", "Gargamel"));
+
+        return ResponseEntity.ok().body(students);
+    }
+
     //Spring Boot REST API with PAth Variable
     @GetMapping("students/{id}/{firstName}/{lastName}")
-    public Student studentPathVariable(@PathVariable("id") int studentId,
+    public ResponseEntity<Student> studentPathVariable(@PathVariable("id") int studentId,
                                        @PathVariable String firstName,
                                        @PathVariable String lastName){
-        return new Student((long) studentId, firstName, lastName);
+        return ResponseEntity.ok(new Student((long) studentId, firstName, lastName));
 
     }
 
     //Spring Boot REST API with Request Param
     //example: http://localhost:8080/students/query?id=1&firstname=Gargamel&lastname=Izreal
     @GetMapping("/students/query")
-    public Student getStudentById(@RequestParam("id")  long studentId,
+    public ResponseEntity<Student> getStudentById(@RequestParam("id")  long studentId,
                                   @RequestParam String firstname,
                                   @RequestParam String lastname){
-        return new Student(studentId, firstname, lastname);
+        Student student = new Student(studentId, firstname, lastname);
+        return ResponseEntity.ok().body(student);
+
     }
 
         //Spring Boot REST API that handle HTTP POST Request - Creating new resource
         // @PostMapping and @RequestBody
     @PostMapping("students/create")
     @ResponseStatus(HttpStatus.CREATED)
-    public Student createStudent(@RequestBody Student student){
+    public ResponseEntity<Student> createStudent(@RequestBody Student student){
         System.out.println(student.getId());
         System.out.println(student.getFirstName() );
         System.out.println(student.getLastName());
-        return student;
+        return new ResponseEntity<>(student, HttpStatus.CREATED);
     }
 
     //String Boot REST API that handle HTTP PUT Request - Updating existing Resoure
     //example: http://localhost:8080/students/76/update
     @PutMapping("students/{id}/update")
-    @ResponseStatus(HttpStatus.OK)
-    public Student updateStudent(@RequestBody Student student, @PathVariable("id") Long studentID){
+    //@ResponseStatus(HttpStatus.OK) <-- Since I added ResponseEntity class and I can put
+    // the HTTP 200 with ok() method , I don't need this anymore
+    public ResponseEntity<Student> updateStudent(@RequestBody Student student, @PathVariable("id") Long studentID){
         System.out.println(student.getFirstName());
         System.out.println(student.getLastName());
         student.setId(77L);
-        return student;
+        return ResponseEntity.ok(student);
     }
 
 
     //String Boot REST API that handle HTTP DELETE Request - deleting existing resource
     //example: http://localhost:8080/students/76/delete
     @DeleteMapping("students/{id}/delete")
-    public String deleteStudent(@PathVariable("id") Long studentID){
-        return "Student with id " + studentID + " deleted";
+    public ResponseEntity<String> deleteStudent(@PathVariable("id") Long studentID){
+        return ResponseEntity.ok("Student with id " + studentID + " deleted");
     }
 
 }
